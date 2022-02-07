1c996abe54b2c1ad8e5870bd103a4af061f33d2f
Mateo Sam <sammateo4@gmail.com>
2022-02-07 16:24:55 -0400

    Connected to firestore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { db } from "./services/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+	const productsCollectionRef = collection(db, "products");
+	useEffect(() => {
+		// db.collection("products");
+
+		const getProducts = async () => {
+			const data = await getDocs(productsCollectionRef);
+			console.log(data);
+			setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+			console.log(data.docs);
+		};
+
+		getProducts();
+	}, []);
+	return (
+		<div className="App">
+			<div>
+				<h1>Product</h1>
+				{products.map((product) => (
+					<div key={product.id}>
+						<p>Name: {product.name}</p>
+						<img src="/chicken-nuggets.jpg" alt="" width="100" height="100" />
+						<p>Description: {product.description}</p>
+						<p>Price: {product.price}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+// import firebase from "firebase";
+// import db from "firebase/db";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: "ecommerce-prototype-de17d",
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
